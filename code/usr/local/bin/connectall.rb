61932d1ffc246fbf16083d17c78f7c4fd6f08d84
Ted Wright <ted@consequential.org>
2024-05-24 08:00:04 -0400

    skip connections TO Linnstrument

--- code/usr/local/bin/connectall.rb
+++ code/usr/local/bin/connectall.rb
@@ -3,19 +3,25 @@
 
 t = `aconnect -i -l`
 ports = []
+linnPort = -1
 t.lines.each do |l|
-  /client (\d*)\:/=~l
+  /client (\d*)\: '(.*)'/=~l
   port = $1
+  name = $2
   # we skip empty lines and the "Through" port
   unless $1.nil? || $1 == '0' || /Through/=~l
     ports << port
-    #names << name
-  end  
+    if name.index("LinnStrument") == 0
+      linnPort = port
+    end
+  end
 end
 
 ports.each do |p1|
   ports.each do |p2|
-    unless p1 == p2 # probably not a good idea to connect a port to itself
+    # probably not a good idea to connect a port to itself
+    # also skip sending to LinnStrument
+    unless p1 == p2 || p2 == String(linnPort)
       system  "aconnect #{p1}:0 #{p2}:0"
     end
   end
